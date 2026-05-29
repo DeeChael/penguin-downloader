fe0eae693b7a01dad16469e60c57e0d5a9b3f831
deechael <DeeChael@outlook.com>
2026-05-29 09:36:09 +0800

    build: bump version to 4.0.1

--- src/core.rs
+++ src/core.rs
@@ -30,7 +30,7 @@ impl PenguinCore {
     /// 版本号命名规则：x.y.z 对应数字 xyyyzzz。
     /// 例如 4.0.0 对应 4000000。
     pub fn get_version(&self) -> i64 {
-        4000000
+        4000001
     }
 
     /// 列出所有已注册的音源提供者。
